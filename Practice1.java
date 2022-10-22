8b46bb71218897f28410c2670d1a006912ccd94f
DaphineKamusiime <107240847+DaphineKamusiime@users.noreply.github.com>
2022-10-22 04:22:50 -0700

    Add files via upload

--- Practice1.java
+++ Practice1.java
@@ -1,13 +1,27 @@
 import java.util.Scanner;
 
 public class Practice1{
-    public static void main(String[] args){
-        Scanner obj=new Scanner(System.in);
-        System.out.println("enter name");
+    public static void main( String[] args){
+        
+        Scanner obj= new Scanner(System.in);
 
-        String userName=obj.nextLine();// reading user input
-        System.out.println("user name: " + userName);
+        System.out.println("ENTER NAME, AGE, SALARY");
+
+        String name=obj.nextLine();
+
+        int age= obj.nextInt();
+
+        double salary= obj.nextDouble();
+
+        System.out.println("NAME: "+ name);
+
+        System.out.println("AGE: " + age);
+
+        System.out.println("SALARY: "+ salary);
+
     }
+
+
 
 
 

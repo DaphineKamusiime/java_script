2d5a5a1deb46ae90c36b3113c4166d3f3a2c3e10
DaphineKamusiime <107240847+DaphineKamusiime@users.noreply.github.com>
2023-05-06 13:13:19 +0300

    Update Practice1.java

--- Practice1.java
+++ Practice1.java
@@ -1,26 +1,4 @@
-import java.util.Scanner;
-
-public class Practice1{
-    public static void main( String[] args){
-        
-        Scanner obj= new Scanner(System.in);
-
-        System.out.println("ENTER NAME, AGE, SALARY");
-
-        String name=obj.nextLine();
-
-        int age= obj.nextInt();
-
-        double salary= obj.nextDouble();
-
-        System.out.println("NAME: "+ name);
-
-        System.out.println("AGE: " + age);
-
-        System.out.println("SALARY: "+ salary);
-
-    }
-    
+   //HACKER RANK
     function getSecondLargest(nums) {
   // Sort the array in descending order
   nums.sort((a, b) => b - a);

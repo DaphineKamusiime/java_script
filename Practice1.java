6c7a2325721506d860e1efc44bc8b30e9c155471
DaphineKamusiime <107240847+DaphineKamusiime@users.noreply.github.com>
2023-05-06 13:12:06 +0300

    Update Practice1.java

--- Practice1.java
+++ Practice1.java
@@ -20,4 +20,20 @@ public class Practice1{
         System.out.println("SALARY: "+ salary);
 
     }
+    
+    function getSecondLargest(nums) {
+  // Sort the array in descending order
+  nums.sort((a, b) => b - a);
+
+  // Traverse the sorted array and return the second element
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[0]) {
+      return nums[i];
+    }
+  }
+
+  // If there is no second largest element, return null
+  return null;
+}
+
 }
